b0b6f2c266d00e9de70fefe03b5289d62524e707
Harry Ward-Gray <wardgray24@gmail.com>
2023-09-13 16:47:17 +0100

    basic device location

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import * as Location from "expo-location";
 export default function App() {
+  function requestPermission() {
+    return Location.requestForegroundPermissionsAsync();
+  }
+
+  const [currentLocation, setCurrentLocation] = useState({});
+
+  useEffect(() => {
+    requestPermission()
+      .then(({ status }) => {
+        if (status !== "granted") throw new Error("permission not granted");
+        Location.getCurrentPositionAsync()
+          .then(({ coords }) => {
+            setCurrentLocation(coords);
+            console.log(coords);
+          })
+          .catch(console.warn);
+      })
+      .catch(console.warn);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontWeight: "bold", fontSize: 30 }}>Test</Text>
+      {Object.keys(currentLocation).map((key) => {
+        return <Text>{`${key} : ${currentLocation[key]}`}</Text>;
+      })}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
